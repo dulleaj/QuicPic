f7b0609238bf4446bdb9efa730589f8c93543272
Drew Dulle <dulleaj@gmail.com>
2016-06-22 20:33:14 -0400

    First commit

---
+++ QuicPic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuicPic
+//
+//  Created by Andrew Dulle on 6/22/16.
+//  Copyright © 2016 Andrew Dulle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuicPic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuicPic
+//
+//  Created by Andrew Dulle on 6/22/16.
+//  Copyright © 2016 Andrew Dulle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
